8aef9457c39dfbc53feb57e08035a200fc91d9ed
Valeria Azurduy <valeria.azurduy@amphinicy.com>
2026-03-25 17:57:20 +0100

    added last task

---
+++ cyber-assessment/5-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+username = sys.argv[1]
+password = sys.argv[2]
+
+response = requests.get('https://api.github.com/user', auth=(username, password))
+if response.status_code == 200:
+    print(response.json()['id'])
+else:
+    print(None)
